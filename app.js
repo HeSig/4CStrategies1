41517077288da9642e47ff797774fa8de6e799a5
HeSig <henrik.sigeman@hotmail.se>
2021-08-10 10:51:44 +0200

    Finalize

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ function GenerateAndReturnByFifteen() {
 
     let numberArray = [];
 
+    //Generate values into the array
     for (let i = 0; i < 16; i++) {
         numberArray.push(Math.floor(Math.random() * 2200) - 1000);
     }
+
+    //Sort the array descending
     numberArray.sort(function (a, b) { return b - a });
     for (let i = 0; i < numberArray.length; i++) {
+        //Print value of current index
         console.log(numberArray[i]);
+        //If the value is divideable by 15, increment the return value by 1
         if (numberArray[i] % 15 == 0) {
             divideableByFifteen++;
         }
